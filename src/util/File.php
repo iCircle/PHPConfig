d268248d59774daf6f3571dc21b88d7460f770f3
RaaghuKR <raghavendra@icircle.co.in>
2015-11-06 00:56:28 +0530

    added copy , move , delete functions for directory and files

--- src/util/File.php
+++ src/util/File.php
@@ -14,12 +14,57 @@ class File{
         if(is_dir($path)){
             $children = array_diff(scandir($path),array('.','..'));
             foreach($children as $child){
-                self::deletePath($path.'/'.$child,$force);
+                self::delete($path.'/'.$child,$force);
             }
         }
     }
 
-    private static function deletePath($path,$force = false){
+    static function copy($source,$target,$force = false){
+        if(file_exists($source)){
+            $source = realpath($source);
+            if($force){
+                chmod(dirname($source),0777);
+                chmod($source,0777);
+            }
+            if(is_dir($source)){
+                if(!file_exists($target)){
+                    // create target dir if not exists
+                    mkdir($target,0777,true);
+                }
+                $children = array_diff(scandir($source),array('.','..'));
+                foreach($children as $child){
+                    self::copy($source.'/'.$child,$target.'/'.$child,$force);
+                }
+            }elseif(is_file($source)) {
+                copy($source,$target);
+            }
+        }
+    }
+
+    static function move($source,$target,$force = false){
+        if(file_exists($source)){
+            $source = realpath($source);
+            if($force){
+                chmod(dirname($source),0777);
+                chmod($source,0777);
+            }
+            if(is_dir($source)){
+                if(!file_exists($target)){
+                    // create target dir if not exists
+                    mkdir($target,0777,true);
+                }
+                $children = array_diff(scandir($source),array('.','..'));
+                foreach($children as $child){
+                    self::move($source.'/'.$child,$target.'/'.$child,$force);
+                }
+                rmdir($source);
+            }elseif(is_file($source)) {
+                rename($source,$target);
+            }
+        }
+    }
+
+    static function delete($path,$force = false){
         if(file_exists($path)){
             $path = realpath($path);
             if($force){
@@ -29,7 +74,7 @@ class File{
             if(is_dir($path)){
                 $children = array_diff(scandir($path),array('.','..'));
                 foreach($children as $child){
-                    self::deletePath($path.'/'.$child);
+                    self::delete($path.'/'.$child,$force);
                 }
                 rmdir($path);
             }elseif(is_file($path)) {
