dbd101e5e7c95759e34c9039413e994d7a212263
RaaghuKR <raghavendra@icircle.co.in>
2015-11-06 00:20:55 +0530

    added $force option while deleteing files

--- src/util/File.php
+++ src/util/File.php
@@ -10,18 +10,22 @@ namespace icircle\util;
 
 class File{
 
-    static function emptyDir($path){
+    static function emptyDir($path,$force = false){
         if(is_dir($path)){
             $children = array_diff(scandir($path),array('.','..'));
             foreach($children as $child){
-                self::deletePath($path.'/'.$child);
+                self::deletePath($path.'/'.$child,$force);
             }
         }
     }
 
-    private static function deletePath($path){
+    private static function deletePath($path,$force = false){
         if(file_exists($path)){
             $path = realpath($path);
+            if($force){
+                chmod(dirname($path),0777);
+                chmod($path,0777);
+            }
             if(is_dir($path)){
                 $children = array_diff(scandir($path),array('.','..'));
                 foreach($children as $child){
